3650d122703ff8f573f622b738a47b594fa54d60
xudafeng <xudafeng@126.com>
2017-06-06 20:46:54 +0800

    add detect-port

--- test/uitest.test.js
+++ test/uitest.test.js
@@ -15,11 +15,12 @@
 
 const uitest = require('..');
 const ipv4 = require('ipv4');
+const detect = require('detect-port');
 
 describe('test', function() {
   this.timeout(300 * 1000);
   it('test', function *() {
-    const port = 8081;
+    const port = yield detect(8081);
     yield uitest({
       url: `http://${ipv4}:${port}/test/`,
       width: 400,
