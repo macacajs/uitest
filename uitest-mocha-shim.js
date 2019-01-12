463983199fb2d437de6a566e84d5f23d371730a4
xudafeng <xudafeng@126.com>
2019-01-12 17:32:11 +0800

    feat: supprt stringify

--- uitest-mocha-shim.js
+++ uitest-mocha-shim.js
@@ -23,6 +23,38 @@
   }
 
   window._macaca_uitest = {
+    stringify: function(obj, replacer, spaces, cycleReplacer) {
+      return JSON.stringify(obj, this.serializer(replacer, cycleReplacer), spaces);
+    },
+
+    serializer: function(replacer, cycleReplacer) {
+      var stack = [];
+      var keys = [];
+
+      cycleReplacer = cycleReplacer || function(key, value) {
+        if (stack[0] === value) {
+          return '[Circular ~]';
+        }
+        return `[Circular ~.${keys.slice(0, stack.indexOf(value)).join('.')}]`;
+      };
+
+      return function(key, value) {
+        if (stack.length) {
+          var thisPos = stack.indexOf(this);
+          ~thisPos ? stack.splice(thisPos + 1) : stack.push(this);
+          ~thisPos ? keys.splice(thisPos, Infinity, key) : keys.push(key);
+
+          if (~stack.indexOf(value)) {
+            value = cycleReplacer.call(this, key, value);
+          }
+        } else {
+          stack.push(value);
+        }
+
+        return replacer == null ? value : replacer.call(this, key, value);
+      };
+    },
+
     screenshot: function(name, cb) {
       if (!isElectron) {
         return cb();
