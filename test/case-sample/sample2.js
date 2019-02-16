a7240d42a8238571a175986a057b8dd6dd8a4905
xudafeng <xudafeng@126.com>
2019-02-16 16:22:59 +0800

    test: supplement testcase

--- test/case-sample/sample2.js
+++ test/case-sample/sample2.js
@@ -5,6 +5,22 @@ const { assert } = chai;
 describe('test/case-sample/sample2.js', function() {
   this.timeout(30 * 1000);
 
+  before(async () => {
+    const p = new Promise((resolve, reject) => {
+      resolve('res');
+    });
+    const res = await p;
+    console.log('before', res);
+  });
+
+  beforeEach(() => {
+    console.log('beforeEach');
+  });
+
+  after(() => {
+    console.log('after');
+  });
+
   it('should be ok', async () => {
     const p = new Promise((resolve, reject) => {
       resolve('res');
