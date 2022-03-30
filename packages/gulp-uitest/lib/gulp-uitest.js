e2bfaa70e642c08e5c24454b3b6ec2e7ec2a2807
达峰的夏天 <xudafeng@126.com>
2022-03-30 13:27:08 +0800

    feat: add gulp (#27)

---
+++ packages/gulp-uitest/lib/gulp-uitest.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const uitest = require('uitest');
+const through = require('through2');
+
+module.exports = function(options) {
+  return through.obj(function(file, env, cb) {
+    uitest(Object.assign({
+      url: 'file://' + file.path
+    }, options))
+      .then(cb, cb);
+  });
+};
